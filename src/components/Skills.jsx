dd9e4a5a651d5e6784ac423771247c53055be98b
Emre Kağan Erde <emrekaganerde@outlook.com>
2025-02-23 16:56:43 +0300

    Complete Skills component design

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,25 +3,29 @@ import React from "react";
 const Skills = () => {
   return (
     <section className="mt-20">
-      <p className="text-5xl">Skills</p>
+      <p className="text-5xl font-semibold">Skills</p>
       <div className="flex justify-between skills-list mt-10">
         <div className="skill">
-          <p className="text-3xl mb-6">JavaScript</p>
-          <p className="text-xs break-words w-60">
+          <p className="text-3xl text-eighth-color font-medium mb-6">
+            JavaScript
+          </p>
+          <p className="text-xs text-tertiary-color break-words w-60">
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
             officia deserunt mollit anim id est laborum.
           </p>
         </div>
         <div className="skill">
-          <p className="text-3xl mb-6">React</p>
-          <p className="text-xs break-words w-60">
+          <p className="text-3xl text-eighth-color font-medium mb-6">React</p>
+          <p className="text-xs text-tertiary-color break-words w-60">
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
             officia deserunt mollit anim id est laborum.
           </p>
         </div>
         <div className="skill">
-          <p className="text-3xl mb-6">ASP.NET Core</p>
-          <p className="text-xs break-words w-60">
+          <p className="text-3xl text-eighth-color font-medium mb-6">
+            ASP.NET Core
+          </p>
+          <p className="text-xs text-tertiary-color break-words w-64">
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
             officia deserunt mollit anim id est laborum.
           </p>
